9b345c189bf0d3a44b43a004f95bd43a1367a940
sebastian <sebastianalexis869@gmail.com>
2023-06-24 03:47:56 -0400

    agregando config mysql railway

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,24 +1,38 @@
-
-import * as dotenv from 'dotenv' 
-import {dirname, join} from "path";
+import * as dotenv from "dotenv";
+import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-dotenv.config({path: join(__dirname,"./../../.env")});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+dotenv.config({ path: join(__dirname, "./../../.env") });
 
-
-
-const options = {
-    host: process.env.MYSQL_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.MYSQL_USER ,
-    password: process.env.MYSQL_PASSWORD,
-    port: process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 200,
-    queueLimit: 0,
+if (process.env.DEVELOP === "true") {
 }
 
-export default options
+const options = {
+  host:
+    process.env.DEVELOP === "true"
+      ? process.env.MYSQL_HOST
+      : process.env.MYSQL_HOST_RAILWAY,
+  database:
+    process.env.DEVELOP === "true"
+      ? process.env.DB_NAME
+      : process.env.DB_NAME_RAILWAY,
+  user:
+    process.env.DEVELOP === "true"
+      ? process.env.MYSQL_USER
+      : process.env.MYSQL_USER_RAILWAY,
+  password:
+    process.env.DEVELOP === "true"
+      ? process.env.MYSQL_PASSWORD
+      : process.env.MYSQL_PASSWORD_RAILWAY,
+  port:
+    process.env.DEVELOP === "true"
+      ? process.env.DB_PORT
+      : process.env.MYSQL_PORT_RAILWAY,
+  waitForConnections: true,
+  connectionLimit: 200,
+  queueLimit: 0,
+};
 
+export default options;
